8971b83d0f3b1e6e88d475c55157eac05908a15e
harshareddy95 <reddyvardhan954@gmail.com>
2025-07-29 11:13:15 +0530

    harshareddy

---
+++ sample.py
@@ -0,0 +1,73 @@
+# age = input('Enter the num')
+# age_int =int(age)   
+# if age_int < 10:
+#     print("Child")
+# else:
+#     print("Not a Child")
+
+# print (type age_int)
+
+
+# for i in range(12):
+#     print(i)
+
+# num = int(input("Enter the range:"))
+# for i in range(num):
+#     print (i)
+
+# cars = ["Ferrai","Lamborghini","Mustang","Porsche","LORD ALTO","NANO"]
+# print(len(cars))
+# print(cars[3])
+
+# for i in cars:
+#     print(i)
+
+# for i in range(len(cars)):
+#     print(cars[i])
+
+# my_array = [10, 20, 30]
+
+# try:
+#     print(my_array[5])  # Attempting to access an out-of-bounds index
+# except IndexError:
+#     print("Error: Index out of range. Please check the array size.")
+
+# my_array = [10, 20, 30]
+
+# index = 5
+# try:
+#     value = my_array[index]
+# except IndexError:
+#     value = None  # Default value
+#     print(f"Index {index} is out of range. Returning default value: {value}")
+
+# print(value)
+
+
+# list = ["one","two","three"]
+# try:
+#     print(list[5])  # Attempting to access an out-of-bounds index
+# except Exception as e:
+#     print("Error in the array")
+
+# with open("sample.txt",'w') as file:
+#     file.write("")
+
+# name="reddy"
+# num=37;
+# print(f" {name}+{num}")
+# text=f" {name}+{num}"
+# print(text)
+
+# def print_user_info(name,city):
+#     text=f"name is {name}. and  his city is {city}"
+#     print(text)
+# name=input("enter name:")
+# city=input("enetr city:")
+# print_user_info(name,city)
+    
+# def printtime(date,time):
+#     print(f"date is {date}.and his time is {time}")
+
+
+
